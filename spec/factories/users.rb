9699a380bb49f141c54b8facb273c5119d4c8c31
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-01 20:17:37 +0900

    passwordカラムのバリデーションの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6, mix_case: false) }
+    password              { '1a' +Faker::Internet.password(min_length: 6, mix_case: false) }
     password_confirmation { password }
     last_name             { '大久保' }
     first_name            { '迅' }
