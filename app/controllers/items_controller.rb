709feebfdfd3218c887fd1e1b6d8459448436751
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-03 07:57:35 +0900

    商品出品画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :price, :user)
+  end
+
 end
