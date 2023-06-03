a4fc5957447c137840ebede4e28c60e161308806
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-03 15:52:11 +0900

    ストロングパラメーターの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
+    @item = Item.all
   end
 
   def new
@@ -17,10 +20,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :explanation, :price, :user)
+    params.require(:item).permit(:name, :explanation, :price, :user, :image, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_time_id).merge(user_id:current_user.id)
   end
 
-  def item_image_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-  end
 end
