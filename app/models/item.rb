6b633621cdf6d48e8b3ea09c8fb3b28754c7cc3e
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-03 15:51:22 +0900

    itemテーブルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,16 @@ class Item < ApplicationRecord
   belongs_to :delivery_time
   belongs_to :prefecture
   belongs_to :status
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
+  
+  validates :image, presence: true
+  validates :category_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :delivery_time_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :status_id, presence: true
 end
