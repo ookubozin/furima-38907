8364bff85c687cd77d3796b9e17ba00752afac53
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-11 19:44:53 +0900

    destroyアクションのルーティングを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_item, only: [:edit, :show, :update]
   
-
-
   def index
     @items = Item.all.order('created_at DESC')
   end
