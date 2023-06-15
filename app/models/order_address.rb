97ab81b145ccd86796a9ca2201a8eb77fe9abede
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-15 23:34:26 +0900

    form_withメソッドにバリデーションを設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,3 +1,15 @@
 class OrderAddress
   include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id , :municipalities, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipalities
+    validates :house_number
+    validates :phone_number 
+    validates :order_id
+    validates :user_id
+    validates :item_id
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 end
