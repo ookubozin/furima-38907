859f1381514790786bdb7b8b06fc3340a65e9ac0
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-16 18:41:02 +0900

    フォームの情報を修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -15,6 +15,6 @@ class OrderAddress
 
   def save
     order = Order.create(user_id: user_id: , item_id: item_id)
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, phone_number: phone_number, order_id: order.id)
   end
 end
