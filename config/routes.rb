32be32f05398824f9a6c4c5bbbc4937e4dae132e
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-17 17:11:31 +0900

    ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
-  resources :items, only: [:show] do
+  resources :items do
     resources :orders, only: [:index, :create]
   end
 end
