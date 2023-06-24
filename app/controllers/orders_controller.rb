a696add47393f0dda55796e6c5f116de56b9b4bf
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-24 16:54:01 +0900

    商品購入機能のコントロールーを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
