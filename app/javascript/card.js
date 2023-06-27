a7300fe7571f3fe14a91a272af5d8deb07372ed7
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-27 22:44:00 +0900

    JavaScriptでtokenを生成

---
+++ app/javascript/card.js
@@ -0,0 +1,28 @@
+const pay = () => {
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  
+  numberElement.mount('#card-number');
+  expiryElement.mount('#card-expiry');
+  cvcElement.mount('#card-cvc');
+  
+  const submit = document.getElementById("button");
+
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
+
