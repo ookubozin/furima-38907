c23e4442af6474e208678703767e73312bb5d39b
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-28 23:21:16 +0900

    コントローラーの記述をリファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,12 +9,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -27,4 +22,15 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
+
+  def pay_item
+    pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
