3c20f717b5d8c963318b45db50a0055c593ad17d
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-28 22:27:44 +0900

    tokenの値をモデルに追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id , :municipalities, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id , :municipalities, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
