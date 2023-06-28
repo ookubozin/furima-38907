4bb938b3ed7bd7b9ef11a5de840c4606917cfe74
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-28 22:28:54 +0900

    ストロングパラメーターにtokenを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,6 +19,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 end
