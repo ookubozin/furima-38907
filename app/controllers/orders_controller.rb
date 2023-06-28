47077420290499aadccab14610b4915edc34a7dc
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-06-28 23:16:54 +0900

    決済処理を記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,12 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @order_address.save
       redirect_to root_path
     else
