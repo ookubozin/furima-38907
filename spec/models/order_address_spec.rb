77457559aaca0e2048ce54ef7023bef5b953c2ad
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-07-01 14:36:19 +0900

    トークンに関連するテストコードを設定

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -65,6 +65,11 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Item can't be blank")
       end
+      it 'tokenが空だと保存できないこと' do
+        @order_address.token= ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
