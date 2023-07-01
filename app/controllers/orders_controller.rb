1b929331af1e1f5822a506885ed0cdaa72d7bb1c
ookubozin <126312929+ookubozin@users.noreply.github.com>
2023-07-01 19:19:48 +0900

    売却済みの商品への遷移を不可に

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,22 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_order, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      pay_item
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @order_address.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render :index
     end
@@ -25,13 +30,11 @@ class OrdersController < ApplicationController
     )
   end
 
-  def pay_item
-    pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    Payjp::Charge.create(
-      amount: @item.price,
-      card: order_params[:token],
-      currency: 'jpy'
-    )
+  def set_order
+    @item = Item.find(params[:item_id])
+    unless @item.order.present?
+    else
+      redirect_to root_path
+    end
   end
 end
